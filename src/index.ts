4f681468f09135d984207713e6905cabc8ed58c3
Reno Ren <renorzr@163.com>
2022-10-31 12:57:20 +0800

    command

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import { textSync } from 'figlet';
 const chalk = require('chalk');
 import { program } from 'commander';
@@ -7,12 +9,12 @@ import { config } from 'dotenv';
 
 program
   .version('0.0.1')
-  .description("Create OneBox Environment of Micro-services")
+  .description("Create One-Key Environment of Micro-services")
   .option('-s, --services <path>', 'Services description file', './services.yml')
   .option('-e, --env <path>', 'environment variables file', '.env')
   .parse(process.argv);
 
-console.log(chalk.green(textSync('OneBox', { horizontalLayout: 'full' })));
+console.log(chalk.green(textSync('OKENV', { horizontalLayout: 'full' })));
 
 const { services, env } = program.opts();
 
